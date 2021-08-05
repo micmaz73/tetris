95939ec2b1791b34d8e27925a45371958b086e2b
micmaz73 <micmaz73@gmail.com>
2021-08-05 19:12:35 +0200

    Added initial message popup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GridBoard from './components/GridBoard';
 import NextBlock from './components/NextBlock';
 import ScoreBoard from './components/ScoreBoard';
 import Controls from './components/Controls';
+import MessagePopup from './components/MessagePopup';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <NextBlock />
       <ScoreBoard />
       <Controls />
+      <MessagePopup />
     </div>
   );
 }
---
+++ src/components/MessagePopup.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+// Display message
+export default function MessagePopup(props){
+    return (
+        <div className="message-popup">
+            <h1>Message Title</h1>
+            <p>Message Info ...</p>
+        </div>
+    )
+}
